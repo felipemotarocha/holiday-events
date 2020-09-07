e6c1f16f5be4ce057246a804d6e373f2666cc768
Felipe Rocha <felipemota.rocha@gmail.com>
2020-09-07 00:52:04 -0300

    feat: fetching from api

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,14 +1,30 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import SendIcon from "@material-ui/icons/Send";
+import axios from "axios";
 
 import { Container, Image, SelectContainer } from "./home.styles";
 
 const DAYS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const HomePage: React.SFC = () => {
+  const fetchHolidays = async () => {
+    const {
+      data: {
+        response: { holidays },
+      },
+    } = await axios.get(
+      `https://calendarific.com/api/v2/holidays?&api_key=${process.env.REACT_APP_CALENDARIFIC_API_KEY}&country=US&year=2019`
+    );
+    console.log(holidays);
+  };
+
+  useEffect(() => {
+    fetchHolidays();
+  });
+
   return (
     <Container>
       <Image src={require("../../images/calendar-colour.svg")} />
